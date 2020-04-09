055c0f182cc64eb2d82a8145866c7a7052d274e6
muhammet-mucahit <mucahitaktepe@gmail.com>
2020-04-09 19:27:21 +0300

    Added Basic App

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'j6QE6JQg9dqMkRWkUM3IJZxOfrvO0gVl'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
